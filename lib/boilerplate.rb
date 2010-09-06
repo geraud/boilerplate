32c00a31bb01336e2e02a4279eaed39ba72507bc
Geraud <geraud@gmail.com>
2010-09-06 12:31:07 -0700

    Engines are Railties...

--- lib/boilerplate.rb
+++ lib/boilerplate.rb
@@ -1,15 +1,17 @@
 boilerplate_path = File.expand_path('../../lib', __FILE__)
 $:.unshift(boilerplate_path) if File.directory?(boilerplate_path) && !$:.include?(boilerplate_path)
 
-require 'action_pack'
+require 'active_support'
 require 'active_support/dependencies/autoload'
 
 module Boilerplate
   extend ActiveSupport::Autoload
+  mattr_accessor :config
+
   autoload :Authentication
   autoload :FeedbackHandling
   autoload :Facebook
+
 end
 
 require 'boilerplate/engine'
-require 'boilerplate/railtie'
--- lib/boilerplate/engine.rb
+++ lib/boilerplate/engine.rb
@@ -2,5 +2,38 @@ require 'rails'
 
 module BoilerPlate
   class Engine < ::Rails::Engine
+
+    def test=(value)
+      $stderr.puts 'setting test to ' + value.to_s
+    end
+
+    config.boilerplate = ActiveSupport::OrderedOptions.new
+
+    #  config.boilerplate.singleton_class.tap do |d|
+    #  end
+
+    initializer 'boilerplate.logger' do
+      ActiveSupport.on_load(:boilerplate) { $stderr.puts 'should do something smart!'} # self.logger ||= Rails.logger }
+    end
+
+    initializer 'boilerplate.initialize_framework_caches' do
+      ActiveSupport.on_load(:boilerplate) { $stderr.puts 'nhtaoeunthaoenth' } # {self.cache_store ||= RAILS_CACHE }
+    end
+
+    initializer 'boilerplate.set_configs' do |app|
+      #  paths   = app.config.paths
+      options = app.config.boilerplate
+      # Set some sensible defaults
+      options.disable_facebook ||= false
+
+      $stderr.puts "Disable Facebook: #{options.disable_facebook}"
+
+      ActiveSupport.on_load(:action_controller) do
+        include Boilerplate::Authentication
+        include Boilerplate::FeedbackHandling
+        include Boilerplate::Facebook # unless options.disable_facebook
+      end
+    end
+
   end
 end
