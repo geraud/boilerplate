3b7c2f3e7c7f86d26cb43acaccd0db9ef1d07395
Geraud <geraud@gmail.com>
2010-09-05 19:15:48 -0700

    Simplified source

--- lib/generators/boilerplate/update_js/update_js_generator.rb
+++ lib/generators/boilerplate/update_js/update_js_generator.rb
@@ -2,12 +2,11 @@ require 'rails/generators'
 
 module Boilerplate
   class UpdateJsGenerator < ::Rails::Generators::Base
+    source_root File.expand_path('../../../../../', __FILE__)
+
     def install
       directory 'app/javascripts'
     end
 
-    def self.source_root
-      File.expand_path('../../../../../', __FILE__)
-    end
   end
 end
